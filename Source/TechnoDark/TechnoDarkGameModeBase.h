9dbe33e560b9f9d1ea33bdb5c7b13b284ca41697
Semenyaka Maxim <Semenyaka.maxim@yandex.ru>
2021-03-01 23:37:22 +0300

    Create a new project

---
+++ Source/TechnoDark/TechnoDarkGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "TechnoDarkGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TECHNODARK_API ATechnoDarkGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
